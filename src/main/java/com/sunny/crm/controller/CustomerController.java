aa9314346c12c037f136e59b2a2ffdcbc83802e9
sunny bhardwaj <104976449+Sunny12110867@users.noreply.github.com>
2024-06-19 00:05:22 +0530

    Update CustomerController.java

--- src/main/java/com/sunny/crm/controller/CustomerController.java
+++ src/main/java/com/sunny/crm/controller/CustomerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/customers")
 @Transactional
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://crmapplication-sunny12110867s-projects.vercel.app/")
 public class CustomerController {
 
     @Autowired
